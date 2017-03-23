0cf060f4af53e69192668bef1a8871faf2628f56
Asanga Udugama <adu@comnets.uni-bremen.de>
2017-03-23 13:50:52 +0100

    fixes 02 - before release

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,18 @@ source ./tools/shell-functions.sh
 loadSettings
 
 if [ "$INET_BUILD" = true ]; then
-    echo "Building inet"
-    cd ../inet
+    echo "Getting the latest version of OMNeT++ INET Framework..."
+    if [ ! -d "modules" ]; then
+        mkdir modules
+    fi    
+    cd modules
+    git submodule add https://github.com/inet-framework/inet.git
+    cd inet
+    git submodule init
+    git submodule update
+    echo "Building the OMNeT++ INET Framework..."
     make makefiles
     make MODE=release
 else
-    echo "Skipping INET"
+    echo "Skipping OMNeT++ INET Framework"
 fi
