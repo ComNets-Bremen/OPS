6f0cac44fed09a5d0106569c32c0f96d264798fc
Jens Dede <jd@comnets.uni-bremen.de>
2018-02-28 12:11:32 +0100

    nits

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,8 +26,7 @@ if [ "$INET_BUILD" = true ]; then
     git submodule add https://github.com/inet-framework/inet.git
     cd inet
     git checkout tags/v3.6.3
-    git submodule init
-    git submodule update
+    git submodule update --init --recursive
     echo "Building the OMNeT++ INET Framework..."
     if [ ! -f “Makefile” ]; then
         make clean
